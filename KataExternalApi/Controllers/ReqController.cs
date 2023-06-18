fdb146debe27ac50783fc2e158ecaa6d2900d425
asalasher <asalasher@yahoo.es>
2023-06-18 17:14:18 +0200

    add features

--- KataExternalApi/Controllers/ReqController.cs
+++ KataExternalApi/Controllers/ReqController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
 
@@ -54,6 +55,67 @@ namespace KataExternalApi.Controllers
             {
                 return BadRequest($"There was an error getting out forecast {response.ReasonPhrase}");
             }
+        }
+
+        /// <summary>
+        /// Requesting information to an api with post method and content in the body
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("api/req/v3")]
+        public async Task<IHttpActionResult> GetV3()
+        {
+            var client = new HttpClient();
+
+            // option 1
+            string jsonBody = JsonConvert.SerializeObject(new
+            {
+                userId = 1,
+                id = 1,
+                title = "new title",
+            });
+
+            //StringContent requestBody = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+
+            // option 2
+            StringContent requestBody = new StringContent(
+                JsonConvert.SerializeObject(
+                    new
+                    {
+                        userId = 1,
+                        id = 1,
+                        title = "new title",
+                    }),
+                Encoding.UTF8,
+                "application/json"
+            );
+
+
+            HttpResponseMessage response = await client.PostAsync("https://fakestoreapi.com/products", requestBody);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return Ok("Succesful request");
+            }
+            else
+            {
+                return BadRequest($"There was an error getting out forecast {response.ReasonPhrase}");
+            }
+
+        }
+
+        /// <summary>
+        /// Reading query params
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("api/req/v4")]
+        public IHttpActionResult GetV4()
+        {
+            string queryParam1 = Request.GetQueryNameValuePairs().FirstOrDefault(q => q.Key == "param1").Value;
+            string queryParam2 = Request.GetQueryNameValuePairs().FirstOrDefault(q => q.Key == "param2").Value;
+
+            return Ok(new { queryParam1, queryParam2, page = 2 });
         }
 
         // GET: api/Req/5
